71827ff31c071d9bf69b4193b7fde9f15ef2b046
naijamesz <kodlikejamesz@hotmail.com>
2024-01-21 23:03:31 +0700

    Edit comments.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 
 import searchImages from './api';
 
-// เรียกใช้แบบฟังก์ชั่น callback
+// Callback function to execute when mutations are observed
 searchImages();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
