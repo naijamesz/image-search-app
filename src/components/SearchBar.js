90f40d6085377fd9bb1f0acb7669625beeee94b5
naijamesz <kodlikejamesz@hotmail.com>
2024-01-22 15:45:57 +0700

    Edit styles.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,21 +5,25 @@ import { useState } from 'react';
 export default function SearchBar({ onSubmit }) {
   const [term, setTerm] = useState('');
 
+  /* handleFormSubmit (after press Enter key to start event) communicate from Child up to Parent(App.js) component with onSubmit props   */
   const handleFormSubmit = event => {
+    /*event.preventDefault(); for disable reload page after user types in input  */
     event.preventDefault();
-
     onSubmit(term);
   };
-
+  /* Handling text inputs */
   const handleChange = event => {
     setTerm(event.target.value);
   };
+
+  /* User types in input > Browser updates the text in the input > Browser triggers an event to the input was updated > React detects  value from the input and update state > State update and Component rerenders > provide 'value' prop and using term to input > loop back to User types in input */
 
   return (
     <div className='search-bar'>
       <form className='form-input' onSubmit={handleFormSubmit}>
         <label>Search any images</label>
-        <input type={term} onChange={handleChange} />
+        {/* <input value={term} onChange={handleChange} /> */}
+        <input type={term} value={term} onChange={handleChange} />
       </form>
     </div>
   );
