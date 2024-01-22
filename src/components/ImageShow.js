98c9d8e92f95a236d5ee249c1d4b7b7462b2e5e8
naijamesz <kodlikejamesz@hotmail.com>
2024-01-22 17:26:50 +0700

    Refactor styles.

--- src/components/ImageShow.js
+++ src/components/ImageShow.js
@@ -1,7 +1,7 @@
 export default function ImageShow({ image }) {
   return (
     <div>
-      <img src={image.urls.small} alt={image.alt_description} />
+      <img src={image.urls.regular} alt={image.alt_description} />
     </div>
   );
 }
